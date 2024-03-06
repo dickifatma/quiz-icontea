2009540294a1cc2c5ba05418dd44de0cdd49020f
dickifatma <dickifatma.dev@gmail.com>
2024-03-06 10:42:03 +0700

    Quiz Apps

---
+++ start.js
@@ -0,0 +1,9 @@
+const greet = require ('henlo-tea')
+
+
+function helloTea()  {
+    // console.log(emoji.random({n: 1, group: 'Food & Drink'})[0])
+    console.log(greet)
+}
+
+module.exports = helloTea
